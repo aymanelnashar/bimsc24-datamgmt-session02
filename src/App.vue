887be7c6e80aefd762d66d25765fc19c918d3e41
aymanelnashar <ayman.elnashar@students.iaac.net>
2024-01-29 19:51:02 +0400

    AymanElNashar-Assignment 02
    Created a button to add text and display on a section of the website
    
    Color changing button not working

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,23 @@
-<!-- the script is where the js code goes -->
+the script is where the js code goes
 <script setup>
-import { ref } from "vue" // you need this to use ref()
+import { Text, ref } from "vue" // you need this to use ref()
 
+    const inputtext = ref("");
+    const displayText = ref("");
+    const selector = ref("");
+    const color = ref("");
 
+    const addText = () => {
+        displayText.value += inputtext.value;
+    };
+
+    const updatecolor = () => {const element = document.querySelector(selector.value);
+        if (element) {
+            element.style.backgroundcolor = color.value;
+            dynamiccolor.value = color.value
+        
+        }
+    }
 </script>
 
 
@@ -10,7 +25,37 @@ import { ref } from "vue" // you need this to use ref()
 <!-- the template is where the html code goes -->
 <template>
 
+    <body>
+    
+    <div id="navbar" class="container">  
+        <div id="title">Ayman website</div>
+        
+        <div id="logo">
+            <img src="./ELEVER 004.jpg" alt="Ayman Chair">
 
+        </div>
+        
+
+    </div>
+
+        <div id="flex">
+
+            <div id="sidebar" class="container"> Sidebar
+
+            <br><input v-model="inputtext" placeholder="Add Text" />
+                    <button @click="addText()">Add Text</button>  
+
+            <br><input v-model="color" type="text" placeholder="Add Color">
+            <button @click="updatecolor" class="container" >Apply Color</button>    
+
+        </div>
+            <div id="main" class="container"> <p>{{ displayText }}</p> </div>
+        
+    </div>
+
+
+    </body>
+    
 
 </template>
 
@@ -19,5 +64,87 @@ import { ref } from "vue" // you need this to use ref()
 <!-- style is where the css code goes -->
 <style scoped>
 
+    html{
+        
+        background-color: rgb(59, 59, 59); 
+        color: white;
+    }
+
+    body{
+
+        margin:0;
+    }
+
+    div{
+
+        /* put the borders in the inside of container */
+        box-sizing: border-box;
+    }
+
+    img{
+
+        height: 100%;
+        width: auto;
+
+    }
+
+    #navbar{
+
+        height: 100px;
+        font-size: 30px;
+        border-color: red;
+
+
+    }
+
+    #flex{
+        
+        display: flex;
+        height: calc(100vh - 50px);
+    }
+
+    #sidebar{
+        
+        width:30%;
+        border-color: blue;
+        border-width: 3px;
+        background-color: rgb(77, 3, 3);
+        font-size: 30px;
+
+    }
+
+    #main{
+        width:70%;
+        border-color: rgb(2, 49, 2);
+        border-width: 3px;
+        background-color: rgb(155, 155, 155);
+        font-size: larger;
+        color: rgb(0, 0, 0);
+
+    }
+
+    #title{
+        width: 70%;
+        float:left;
+    }
+
+    #logo{
+        width: 30%;
+        height: 100%;
+        float:right;
+        text-align: right;
+
+    }
+
+    .container{
+
+        border-style: dotted;
+        border-width: 3px;
+        background-color: rgb(0, 0, 0);
+        font-style: normal;
+        color: white;
+
+    }
 
 </style>
+
